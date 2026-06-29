e1320132ba70a619d2cfd96a727c0ee4d1e0d4e5
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-29 12:35:50 +0530

    chore: Add entity entry to configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@routes": path.resolve(__dirname, "./src/routes"),
       "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
       "@config": path.resolve(__dirname, "./src/config"),
     },
   },
