0663644ce3047dc5e16c5c80f88d98d2a17caa86
Akash Kumar R <akashkumar210874@gmail.com>
2026-07-01 13:40:55 +0530

    feat: Add pagination to employeee list

--- client/src/features/employees/services/employeeApi.ts
+++ client/src/features/employees/services/employeeApi.ts
@@ -1,12 +1,14 @@
+import { type PaginatedResult } from "@/shared/type/pagination";
+
 import { type EmployeeResponse } from "@entities/employee/types/apiTypes";
 
 import employeeBaseApi from "@shared/api/base.api";
 
 export const employeeApi = employeeBaseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getEmployees: builder.query<EmployeeResponse[], void>({
-      query: () => ({
-        url: "/employees",
+    getEmployees: builder.query<PaginatedResult<EmployeeResponse>, string>({
+      query: (params) => ({
+        url: `/employees?${params}`,
         method: "GET",
       }),
     }),
