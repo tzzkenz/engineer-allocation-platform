a600ac0902763ed84460197422a45d0e0417315b
Kenz E S <kenznavas@gmail.com>
2026-07-01 16:49:17 +0530

    feat: findresources for agent done

--- server/app/features/agent/tools/employee_tools.py
+++ server/app/features/agent/tools/employee_tools.py
@@ -5,14 +5,18 @@ from features.employee.service import EmployeeService
 def make_employees_tools(employee_service: EmployeeService):
 
     @tool
-    async def find_available_resources():
+    async def find_resources(
+        skill: str | None = None, check_for_available: bool = False
+    ):
         """
-        List out all the employees with their names
+        List out all the employees with their names, roles,optionally filtered based on skill name. Also returns the number of prjects he is part of.
         """
 
         employees_with_role = (
-            await employee_service.list_all_employees_with_roll_for_agent()
+            await employee_service.list_all_employees_with_roll_for_agent(
+                skill, check_for_available
+            )
         )
         return employees_with_role
 
-    return [find_available_resources]
+    return [find_resources]
